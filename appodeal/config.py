1f628181f24e99c661d2e3bd4c2cc512a2561b3b
Shin-NiL <niltonr@gmail.com>
2015-12-06 22:01:27 -0200

    Fixed applovin reference

--- appodeal/config.py
+++ appodeal/config.py
@@ -5,7 +5,7 @@ def configure(env):
     env.android_module_file("android/GodotAppodeal.java")
     env.android_module_manifest("android/AndroidManifestChunk.xml")
     env.android_module_library("android/sdk/android-support-v4-22.2.1.jar")
-    env.android_module_library("android/sdk/applovin-sdk-6.1.4.jar")
+    env.android_module_library("android/sdk/applovin-6.1.4.jar")
     env.android_module_library("android/sdk/appodeal-1.13.10.jar")
     env.android_module_library("android/sdk/chartboost-6.0.2.jar")
     env.android_module_library("android/sdk/my-target-4.1.2.jar")
