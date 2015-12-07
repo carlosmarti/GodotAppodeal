a4835917565aada66a1471fb2e54a97e27f39fa3
Carlos Marti <carlos.marti29@yahoo.com>
2015-12-06 23:17:50 -0500

    added example

---
+++ appodeal-example/Ads.gd
@@ -0,0 +1,48 @@
+
+extends Node
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+var appodeal = null
+var key = null
+
+func _ready():
+	# Initialization here
+	if(Globals.has_singleton("Appodeal")):
+		key = "your key"
+		appodeal = Globals.get_singleton("Appodeal")
+		appodeal.init(key, "normal")
+
+func showBannerAd(type):
+	appodeal.showBannerAd(type)
+
+func showVideoAd():
+	appodeal.showVideoAd()
+
+func showInterstitialAd(): 
+	appodeal.showInterstitialAd()
+
+func showInterstitialAndVideoAds():
+	appodeal.showInterstitialAndVideoAds()
+
+func hideBannerAd():
+	appodeal.hideBannerAd()
+
+func hideVideoAd():
+	appodeal.hideVideoAd()
+
+func hideInterstitialAd():
+	appodeal.hideInterstitialAd()
+
+func isBannerLoaded():
+	return appodeal.isBannerLoaded()
+
+func isVideoLoaded():
+	return appodeal.isVideoLoaded()
+
+func isInterstitialLoaded():
+	return appodeal.isInterstitialLoaded()
+
+func isAnyAdLoaded():
+	return appodeal.isAnyAdLoaded()
