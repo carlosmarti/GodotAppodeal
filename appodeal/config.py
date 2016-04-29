5d87eb662ea282f2e5d974967815085eeb65531d
Shin-NiL <niltonr@gmail.com>
2016-04-29 18:33:41 -0300

    Updated code to compile to appodeal SDK 1.14.12

--- appodeal/config.py
+++ appodeal/config.py
@@ -5,5 +5,5 @@ def configure(env):
 	if (env['platform'] == 'android'):
 		env.android_add_java_dir("android/src")
 		env.android_add_to_manifest("android/AndroidManifestChunk.xml")
-		env.android_add_dependency("compile 'com.google.android.gms:play-services-ads:+'")
+		env.android_add_dependency("compile 'com.google.android.gms:play-services-ads:8.4.0'")
 		env.android_add_dependency("compile fileTree(dir: '../../../modules/appodeal/android/libs', include: '*.jar')")
